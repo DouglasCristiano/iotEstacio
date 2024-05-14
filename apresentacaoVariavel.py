32e7db14c4c5983962f36379a380ad727f5db087
DouglasCristiano <dcristiano.silva@outlook.com>
2024-05-14 20:52:36 -0300

    aula01

---
+++ apresentacaoVariavel.py
@@ -0,0 +1,16 @@
+print(15 + 4)
+# soma
+
+print(15 - 4)
+#subtracao
+
+print(15 / 4)
+#divisao
+
+print(15 * 4)
+#multiplicacao
+
+altura = 1.80
+peso = 95
+imc = peso / (altura * altura)
+linha_1 = f'tem altura de {altura:.2f} pesa {peso} quilos e seu IMC é {imc:.2f} '
